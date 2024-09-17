c481eed5ad343c8ac35aef4bc66a36f92cc0c432
doranl <doranl@educ.college-montreal.qc.ca>
2024-09-17 16:18:38 -0400

    ajout TP2.py (final)

--- TP2.py
+++ TP2.py
@@ -4,8 +4,15 @@ Nom: Liam Doran
 Groupe: 404
 """
 import random
-minimum = 1
-maximum = 1000
+minimum = 0
+maximum = 100
+def choix_usager(minimum, maximum):
+    minimum = int(input("choisi un nombre. ce sera le minimum"))
+    maximum = int(input("choisi un nombre. ce sera le maximum"))
+    return (minimum, maximum)
+
+minimum, maximum = choix_usager(minimum, maximum)
+
 jouer = 0
 jeu = "a"
 while jouer == 0:
@@ -13,7 +20,6 @@ while jouer == 0:
     print(f"J’ai choisi un nombre au hasard entre {minimum} et {maximum}.\nÀ vous de le deviner...\n")
     choix2 = random.randint(minimum, maximum)
     while choix != choix2:
-        print(choix2)
         choix = int(input("Entrez votre essai :   "))
         if choix > choix2:
             print("%d est plus grand" % choix)
