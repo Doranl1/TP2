eb4b397659398a81666adfcbb0318bcc88603e88
doranl <doranl@educ.college-montreal.qc.ca>
2024-09-17 15:58:41 -0400

    ajout TP2.py (final)

--- TP2.py
+++ TP2.py
@@ -1,19 +1,19 @@
 import random
-min = 1
-max = 1000
+minimum = 1
+maximum = 1000
 jouer = 0
 jeu = "a"
 while jouer == 0:
     choix = -1
-    print("J’ai choisi un nombre au hasard entre %d et %d.\nÀ vous de le deviner...\n" %(min, max))
-    choix2 = random.randint(min, max)
+    print(f"J’ai choisi un nombre au hasard entre {minimum} et {maximum}.\nÀ vous de le deviner...\n")
+    choix2 = random.randint(minimum, maximum)
     while choix != choix2:
         print(choix2)
         choix = int(input("Entrez votre essai :   "))
         if choix > choix2:
-            print("%d est plus grand" %(choix))
+            print("%d est plus grand" % choix)
         elif choix < choix2:
-            print("%d est plus petit"%(choix))
+            print("%d est plus petit" % choix)
         elif choix == choix2:
             print("bravo...")
             choix = choix2
