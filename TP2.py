91a89bec18c5cfed0fbf70e5e8ca7c6c654447ae
doranl <doranl@educ.college-montreal.qc.ca>
2024-09-26 10:20:44 -0400

    (final)

--- TP2.py
+++ TP2.py
@@ -10,24 +10,27 @@ jeu = 0
 
 
 while jeu == 0:
+    # choisir si l'usager veut choisir le borne
     borne_usager = str(input("voulez-vous choisir le minimum et maximum?\n"))
     if borne_usager == "oui":
         borne_minimum = int(input("quel est le minimum?\n"))
         borne_maximum = int(input("quel est le minimum?\n"))
     else:
         print("\n")
+    # intro du jeu
     choix = -1
     print(f"J’ai choisi un nombre au hasard entre {borne_minimum} et {borne_maximum}.\nÀ vous de le deviner...\n")
     choix2 = random.randint(borne_minimum, borne_maximum)
-    while choix != choix2:
+    while choix != choix2:  # début du jeu
         choix = int(input("Entrez votre essai :\n"))
-        if choix > choix2:
+        if choix > choix2:  # le choix est grande
             print("%d est plus grand" % choix)
-        elif choix < choix2:
+        elif choix < choix2:  # le chois est petit
             print("%d est plus petit" % choix)
-        elif choix == choix2:
+        elif choix == choix2:  # la victiore de l'usager
             print("bravo, vous avez gagner")
             choix = choix2
+    # rejouer
     jeu = str(input("veut-tu rejouer?"))
     if jeu == "oui" or jeu == "Oui":
         jeu = 0
