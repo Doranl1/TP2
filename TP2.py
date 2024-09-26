50a2467edef74874defc22a0a25d929d983a4094
doranl <doranl@educ.college-montreal.qc.ca>
2024-09-26 09:48:30 -0400

    (final)

--- TP2.py
+++ TP2.py
@@ -4,33 +4,33 @@ Nom: Liam Doran
 Groupe: 404
 """
 import random
-minimum = 0
-maximum = 100
-def choix_usager(minimum, maximum):
-    minimum = int(input("choisi un nombre. ce sera le minimum"))
-    maximum = int(input("choisi un nombre. ce sera le maximum"))
-    return (minimum, maximum)
+borne_minimum = 0
+borne_maximum = 100
+jeu = 0
 
-minimum, maximum = choix_usager(minimum, maximum)
 
-jouer = 0
-jeu = "a"
-while jouer == 0:
+while jeu == 0:
+    borne_usager = str(input("voulez-vous choisir le minimum et maximum?\n"))
+    if borne_usager == "oui":
+        borne_minimum = int(input("quel est le minimum?\n"))
+        borne_maximum = int(input("quel est le minimum?\n"))
+    else:
+        print("\n")
     choix = -1
-    print(f"J’ai choisi un nombre au hasard entre {minimum} et {maximum}.\nÀ vous de le deviner...\n")
-    choix2 = random.randint(minimum, maximum)
+    print(f"J’ai choisi un nombre au hasard entre {borne_minimum} et {borne_maximum}.\nÀ vous de le deviner...\n")
+    choix2 = random.randint(borne_minimum, borne_maximum)
     while choix != choix2:
-        choix = int(input("Entrez votre essai :   "))
+        choix = int(input("Entrez votre essai :\n"))
         if choix > choix2:
             print("%d est plus grand" % choix)
         elif choix < choix2:
             print("%d est plus petit" % choix)
         elif choix == choix2:
-            print("bravo...")
+            print("bravo, vous avez gagner")
             choix = choix2
     jeu = str(input("veut-tu rejouer?"))
     if jeu == "oui" or jeu == "Oui":
         jeu = 0
     else:
-        print("ok")
+        print("bye")
         break
